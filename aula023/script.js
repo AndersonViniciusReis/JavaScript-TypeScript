b462f6ba23f87cd4d8e4364f6e0a96898d2057cd
Anderson Vinicius Reis Moreira <108693673+AndersonViniciusReis@users.noreply.github.com>
2026-06-02 11:13:26 -0300

    criei a aula023

---
+++ aula023/script.js
@@ -0,0 +1,17 @@
+//avaliacão de curto circuito
+
+//valores naturalmente avaliados como falsy
+/*falsy
+ * false
+ * 0
+ * null
+ * undefined
+ * NaN
+ * ' ' " " ` `
+ */
+
+//valores naturalmente avaliados como truthy
+/*truthy
+ * true
+ * 1
+ */
