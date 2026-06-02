4c8847a111fb226c12fdc4b6b457631729d1714c
Anderson Vinicius Reis Moreira <108693673+AndersonViniciusReis@users.noreply.github.com>
2026-06-02 10:58:09 -0300

    criei a aula003

---
+++ aula003/script.js
@@ -0,0 +1,11 @@
+//Strings podem ser delimitadas por aspas simples, duplas ou crases (template literals)
+// strings são iguais texto, palavras, frases, etc. Tudo que estiver entre aspas é considerado uma string
+console.log("Luiz 'Otávio'"); // Aspas duplas permitem aspas simples sem necessidade de escape
+console.log('Luiz "Otávio"'); // Aspas simples permitem aspas duplas sem necessidade de escape
+console.log(`'Luiz' "Otávio"`); // Template literals permitem ambas as formas de aspas
+//number são iguais a números, ou seja, 15 é igual a 15.00, por exemplo. O JavaScript não diferencia inteiros de decimais 
+//virgula é usada para separar os argumentos dentro do console.log, ou seja, cada argumento é separado por vírgula
+//ponto é usado para acessar propriedades e métodos de objetos, ou seja, quando queremos acessar uma propriedade ou método de um objeto, usamos o ponto para separar o nome do objeto do nome da propriedade ou método
+
+console.log(35, 15.85, 'Luiz Otávio de novo'); 
+
