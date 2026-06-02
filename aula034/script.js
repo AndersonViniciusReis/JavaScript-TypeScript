a09cb83ab4662487c815aebc488bbdde82350496
Anderson Vinicius Reis Moreira <108693673+AndersonViniciusReis@users.noreply.github.com>
2026-06-02 11:19:20 -0300

    criei a aula034

---
+++ aula034/script.js
@@ -0,0 +1,61 @@
+/*
+FOR CLÁSSICO
+
+- Usado para repetir um bloco de código várias vezes
+- Possui 3 partes:
+
+1. Inicialização -> onde a variável é criada
+2. Condição -> enquanto for verdadeira o laço continua
+3. Incremento -> altera a variável a cada repetição
+
+Estrutura:
+
+for (inicialização; condição; incremento) {
+
+}
+
+Exemplo:
+
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}
+
+Saída:
+0
+1
+2
+3
+4
+*/
+/*
+FOR CLÁSSICO
+
+- Usado para repetir um bloco de código várias vezes
+- Possui 3 partes:
+
+1. Inicialização -> onde a variável é criada
+2. Condição -> enquanto for verdadeira o laço continua
+3. Incremento -> altera a variável a cada repetição
+
+Estrutura:
+
+for (inicialização; condição; incremento) {
+
+}
+
+Exemplo:
+
+for (let i = 0; i < 5; i++) {
+    console.log(i);
+}
+
+Saída:
+0
+1
+2
+3
+4
+*/
+for (let i = 0; i <= 5; i++) {
+    console.log(`linha ${i}`);
+}
