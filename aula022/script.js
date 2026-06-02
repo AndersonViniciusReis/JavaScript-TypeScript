c3b05dde20c2eccc8cbaa713d7018165c9bb6c36
Anderson Vinicius Reis Moreira <108693673+AndersonViniciusReis@users.noreply.github.com>
2026-06-02 11:13:13 -0300

    criei a aula022

---
+++ aula022/script.js
@@ -0,0 +1,17 @@
+/*operadores logicos
+ * 
+ * && -> AND -> E ->todas as condições devem ser verdadeiras para o resultado ser verdadeiro
+ * || -> OR -> OU -> basta uma condição ser verdadeira para o resultado ser verdadeiro
+ * ! -> NOT -> NÃO -> inverte o valor lógico, se for verdadeiro, torna-se falso, e vice-versa
+ * 
+ */
+console.log(true && true); // true
+console.log(true && false); // false
+console.log(false && true); // false
+console.log(false && false); // false   
+console.log(true || true); // true
+console.log(true || false); // true
+console.log(false || true); // true
+console.log(false || false); // false
+console.log(!true); // false
+console.log(!false); // true
