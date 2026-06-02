272d8620fb3d80692d0f75d56304c032427aae9e
Anderson Vinicius Reis Moreira <108693673+AndersonViniciusReis@users.noreply.github.com>
2026-06-02 13:55:07 -0300

    Atualizei a aula077

--- aula077/script.js
+++ aula077/script.js
@@ -14,9 +14,48 @@ Se o digito for maior que 9, considerar como 0.
 11 - (284 % 11) = 2 (segundo dígito)
 se o digito for maior que 9, considerar como 0.
 */
+function ValidaCPF(cpfEnviado) {
+    Object.defineProperty(this, 'cpfLimpo', {
+        enumerable: true,
+        get: function() {
+            return cpfEnviado.replace(/\D+/g, '');
+        }
+    });
+}
 
-let cpf = '705.484.450-52';
-let cpfLimpo = cpf.replace(/\D/g, ''); // Remove tudo que não for dígito
-console.log(cpfLimpo); // 70548445052
-let cpfArray = Array.from(cpfLimpo).map(Number); // Converte cada caractere para número
-console.log(cpfArray); // [7, 0, 5, 4, 8, 4, 4, 5, 0, 5, 2]
+ValidaCPF.prototype.valida = function() {
+    if (typeof this.cpfLimpo === 'undefined') return false;
+    if (this.cpfLimpo.length !== 11) return false;
+    if (this.isSequencia()) return false;
+
+    const cpfParcial = this.cpfLimpo.slice(0, -2);
+    const digito1 = this.criaDigito(cpfParcial);
+    const digito2 = this.criaDigito(cpfParcial + digito1);
+
+    const novoCpf = cpfParcial + digito1 + digito2;
+    return novoCpf === this.cpfLimpo;
+};
+
+ValidaCPF.prototype.criaDigito = function(cpfParcial) {
+    const cpfArray = Array.from(cpfParcial);
+
+    let regressivo = cpfArray.length + 1;
+    const total = cpfArray.reduce((ac, val) => {
+        ac += (regressivo * Number(val));
+        regressivo--;
+        return ac;
+    }, 0);
+
+    const digito = 11 - (total % 11);
+    return digito > 9 ? '0' : String(digito);
+
+};
+
+ValidaCPF.prototype.isSequencia = function() {
+    const sequencia = this.cpfLimpo[0].repeat(11);
+    return sequencia === this.cpfLimpo;
+};
+
+const cpf = new ValidaCPF('070.987.720-03');
+console.log(cpf.valida());
+
