3a886d8dca0e02ea336670d5c0b1d8f559058ae3
Anderson Vinicius Reis Moreira <108693673+AndersonViniciusReis@users.noreply.github.com>
2026-06-02 10:58:42 -0300

    criei a aula004

---
+++ aula004/comentarios.js
@@ -0,0 +1,15 @@
+// E escrever um comentário
+console.log('Olá Mundo!'); // Aqui tem outro comentário
+console.log('Linha 1');
+// console.log('Linha 2');
+/*
+console.log('Linha 3');
+console.log('Linha 4');
+*/
+//vai ser ignorado
+
+//comentário de uma linha
+
+/*
+ comentário de múltiplas linhas 
+ */
