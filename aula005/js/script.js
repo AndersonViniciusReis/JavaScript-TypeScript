feaa1d54cbfa83f4177bb9098063fc4f070e3723
Anderson Vinicius Reis Moreira <108693673+AndersonViniciusReis@users.noreply.github.com>
2026-06-02 10:59:31 -0300

    criei a aula005

---
+++ aula005/js/script.js
@@ -0,0 +1,3 @@
+console.log('Olá Mundo!');//console.log só exibe a mensagem no console do navegador, não é exibida na tela do usuário
+console.log('este trecho será exibido no console do navegador');
+alert('Olá Mundo!');//alert exibe uma caixa de diálogo com a mensagem para o usuário, é exibida na tela do usuário
